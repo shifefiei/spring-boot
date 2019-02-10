72443757e95c5f59fc7590b035e579e0ccc68023
shifeifei <shifeifei6@163.com>
2019-02-10 19:31:57 +0800

    spring boot以war包的形式部署

--- spring-boot-jdbc/src/main/java/com/sff/spring/boot/jdbc/config/DruidConfig.java
+++ spring-boot-jdbc/src/main/java/com/sff/spring/boot/jdbc/config/DruidConfig.java
@@ -1,6 +1,6 @@
 package com.sff.spring.boot.jdbc.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +10,9 @@ import javax.sql.DataSource;
 @Configurable
 public class DruidConfig {
 
-
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource druid() {
-        return new DruidDataSource();
+    @ConfigurationProperties("spring.datasource.druid")
+    public DataSource dataSourceTwo(){
+        return DruidDataSourceBuilder.create().build();
     }
-
 }
